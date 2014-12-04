40429a2eecbce080eb811fba96f4db9d90b03f2e
Evan Moreira <Evannigel@gmail.com>
2014-12-03 21:03:23 -0500

    Pseudo Code for ID# Decision Tree

--- GuessWho/src/game/gamelogic.py
+++ GuessWho/src/game/gamelogic.py
@@ -31,12 +31,24 @@ def InitialQuestion():
 
 def decisionTree():
     
-    
+    """
+    ID3 (Examples, Target_Attribute, Attributes)
+    Create a root node for the tree
+    If all examples are positive, Return the single-node tree Root, with label = +.
+    If all examples are negative, Return the single-node tree Root, with label = -.
+    If number of predicting attributes is empty, then Return the single node tree Root,
+    with label = most common value of the target attribute in the examples.
+    Otherwise Begin
+        A , The Attribute that best classifies examples.
+        Decision Tree attribute for Root = A.
+        For each possible value, v_i, of A,
+            Add a new tree branch below Root, corresponding to the test A = v_i.
+            Let Examples(v_i) be the subset of examples that have the value v_i for A
+            If Examples(v_i) is empty
+                Then below this new branch add a leaf node with label = most common target value in the examples
+            Else below this new branch add the subtree ID3 (Examples(v_i), Target_Attribute, Attributes ? {A})
+    End
+    Return Root
+    """
     
     return tree
-
-def updateFrequency():
-
-    
-
-    return
